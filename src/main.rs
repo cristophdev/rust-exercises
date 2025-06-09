ee14495441002613a4e078d1a0d8d2a7231527c9
Cristopher <cristoperesc@gmail.com>
2025-06-09 14:34:23 -0500

    gigasecond exercise

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
-mod reverse_string;
+use time::{PrimitiveDateTime, Date, Time};
+mod gigasecond;
 
 fn main() {
-    println!("{}", reverse_string::reverse("hello"));
+    let start = PrimitiveDateTime::new(
+        Date::from_calendar_date(2024, time::Month::June, 9).unwrap(),
+        Time::from_hms(20, 0, 0).unwrap(),
+    );
+
+    println!("{}", gigasecond::after(start));
 }
